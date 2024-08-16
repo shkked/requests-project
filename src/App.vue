f4b006e8ed150d1ec9c9a2b102d4b7cf6b0bdbba
kenny <pletnev735@gmail.com>
2024-08-16 13:52:48 +0300

    add vuetify and some styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
-    
+    <v-container>
+      <headerPage />
+    </v-container>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import headerPage from './components/headerPage.vue'
+export default {
+  name: 'App',
+  components: {
+    headerPage
+  }
+}
+</script>
 
+
+<style lang="scss">
+@import './assets/_main.scss';
+
+#app{
+  background: rgba(238, 238, 238, 1);
+  gap: 20px;
+  height: 100%;
+}
 </style>
---
+++ src/components/headerPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="d-flex">
+        <h1>Список заявок</h1> 
+    </header>
+</template>
+
+<style lang="scss">
+h1{
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 24px;
+    color: rgba(69, 69, 69, 1);
+}
+</style>
